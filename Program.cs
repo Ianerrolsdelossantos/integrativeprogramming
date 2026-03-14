573cec63d4bedd4868ac50014e9d12de7539a780
Ianerrolsdelossantos <ianerrolsdelossantos@gmail.com>
2026-03-14 20:05:37 +0800

    Add files via upload

---
+++ Program.cs
@@ -0,0 +1,55 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Delossantos
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            
+            string[] classmates = { "Anna", "Brian", "Carla", "David", "Ella", "Frank" };
+
+            Console.WriteLine("Classmates:");
+            foreach (string name in classmates)
+            {
+                Console.WriteLine(name);
+            }
+
+            
+            char[,] letters =
+            {
+                { 'A', 'B', 'C' },
+                { 'D', 'E', 'F' }
+            };
+
+            Console.WriteLine("\nTwo-Dimensional Array:");
+            for (int i = 0; i < 2; i++)
+            {
+                for (int j = 0; j < 3; j++)
+                {
+                    Console.Write(letters[i, j] + " ");
+                }
+                Console.WriteLine();
+            }
+
+            
+            string message = "Hello everyone, welcome to the class!";
+
+            if (message.ToLower().Contains("hello"))
+            {
+                Console.WriteLine("\nThe message contains the word 'hello'.");
+            }
+            else
+            {
+                Console.WriteLine("\nThe message does not contain the word 'hello'.");
+            }
+        }
+    }
+}
+        
+    
+
